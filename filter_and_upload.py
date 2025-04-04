0925a09c469a991e18e1175f4e98f60f6a851bd5
ElioErrico <143315119+ElioErrico@users.noreply.github.com>
2025-04-04 18:47:18 +0200

    Update filter_and_upload.py

--- filter_and_upload.py
+++ filter_and_upload.py
@@ -1,5 +1,6 @@
 from cat.mad_hatter.decorators import hook
 import json
+from cat.log import log
 
 @hook  # default priority = 1
 def before_cat_recalls_declarative_memories(declarative_recall_config, cat):
@@ -10,9 +11,16 @@ def before_cat_recalls_declarative_memories(declarative_recall_config, cat):
     user_status_path="cat/static/user_status.json"
     file=open(user_status_path, "r")
     user_status=json.load(file)
+    user_status[user][user] = True
+    # cat.send_ws_message(f"{str(user_status[user])}","chat")
 
     declarative_recall_config["metadata"] = user_status[user]
+    # Filtra il dizionario mantenendo solo le coppie chiave-valore con valore True
+    declarative_recall_config["metadata"]={k: v for k, v in declarative_recall_config["metadata"].items() if v}
     # cat.send_ws_message(f"{str(user_status[user])}","chat")
+    log.critical(f'metadata: {str(declarative_recall_config["metadata"])}')
+
+
 
     return declarative_recall_config
 
